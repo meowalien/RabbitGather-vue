9393f0ccd8e25d0e3ab083c7d3c91ce10b3b3883
jacky_li <jacky_li@Jackys-MacBook-Pro.local>
2023-09-04 12:31:34 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="w-full h-full">
+
   </div>
-  <HelloWorld msg="Vite + Vue" />
+<!--  <div>-->
+<!--      <img src="/rabbit.svg" class="logo vue" alt="Vue logo" />-->
+<!--  </div>-->
 </template>
 
 <style scoped>
+
 .logo {
   height: 6em;
   padding: 1.5em;
