203df6ba26152f44e679c1892ad33989724ec4c6
jacky_li <jacky_li@Jackys-MacBook-Pro.local>
2023-09-07 15:04:11 +0800

    edit WebMonitoringCrawler

---
+++ src/components/InputBar.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+const { name, modelValue } = defineProps(['name', 'modelValue']);
+const emit = defineEmits(['update:modelValue']);
+
+const updateMessage = (value: Event) => {
+  const inputValue = (value.target as HTMLInputElement).value;
+  emit('update:modelValue', inputValue);
+};
+
+</script>
+
+<template>
+  <div class="flex flex-row items-center">
+    <span class="align-baseline">{{name}}:&nbsp;&nbsp;</span>
+    <input class="flex-1 bg-black border-b-1 focus:outline-none" :value="modelValue" @input="updateMessage">
+  </div>
+</template>
--- src/view/WebMonitoringCrawler.vue
+++ src/view/WebMonitoringCrawler.vue
@@ -1,9 +1,26 @@
 <script setup lang="ts">
+import InputBar from "@/components/InputBar.vue";
+import { ref } from 'vue';
 
+// Factory function for input fields
+const createInputFields = () => [
+  { name: "Name", fieldValue: ref("") },
+  { name: "URL", fieldValue: ref("") },
+  { name: "Selector", fieldValue: ref("") },
+  { name: "Check interval", fieldValue: ref("") },
+  { name: "Trigger event", fieldValue: ref("") },
+  { name: "Notify to", fieldValue: ref("") }
+];
+
+const inputFields = createInputFields();
 </script>
-
 <template>
-  <h1>AAAAA</h1>
+  <div class="w-full h-full  border-blue-500 flex flex-col items-center justify-start">
+    <div class="pt-10 max-w-1/2 w-90 flex flex-col items-start justify-start">
+      <InputBar class="w-full" v-for="inputField in inputFields" :key="inputField.name" :name="inputField.name" v-model="inputField.fieldValue.value"></InputBar>
+    </div>
+  </div>
+
 </template>
 
 <style scoped>
